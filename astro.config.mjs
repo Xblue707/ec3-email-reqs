86aec47a4cc927c3456510b6acf1fb125b4398d4
Canaris <mrgeek484@gmail.com>
2025-01-19 17:31:11 +0800

    Add initial project setup with TypeScript, Tailwind CSS, and Alpine.js integrations

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import alpinejs from '@astrojs/alpinejs';
+import tailwind from '@astrojs/tailwind';
+import vue from '@astrojs/vue';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+	output: 'server',
+	integrations: [alpinejs(), vue(), tailwind()],
+});
