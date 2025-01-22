7a716914497b4840a235fdeb9d26f913ee341cce
Canaris <mrgeek484@gmail.com>
2025-01-22 15:07:34 +0800

    Update oauth.ts

--- src/lib/oauth.ts
+++ src/lib/oauth.ts
@@ -1,7 +1,11 @@
 import { Google } from 'arctic';
 
+const siteUrl = import.meta.env.PROD
+	? import.meta.env.SITE
+	: 'http://localhost:4321';
+
 export const google = new Google(
 	import.meta.env.GOOGLE_CLIENT_ID,
 	import.meta.env.GOOGLE_CLIENT_SECRET,
-	'http://localhost:4321/api/login/google/callback',
+	`${siteUrl}/api/login/google/callback`,
 );
